a6a9e9747093e46ddb7b133911ec82b65fa75b04
Александр Александрович Голубев <aag@ganymed.factory.alexplus.ru>
2020-07-30 15:09:35 +0300

    test clock in main f0

--- test/f0_discovery/main.cpp
+++ test/f0_discovery/main.cpp
@@ -22,6 +22,8 @@
 // #include "example/example_modbus_master.h"
 // #include "example_flash.h"
 // #include "example_safe_flash.h"
+#include "string_buffer.h"
+#include "hd44780.h"
 
 
 /// эта функция вызываеться первой в startup файле
@@ -45,25 +47,63 @@ using STEP  = mcu::PA1;
 using Left  = mcu::PA4;
 using Right = mcu::PA5;
 
+using RS    = mcu::PA0;  
+using E     = mcu::PA1;       
+using RW    = mcu::PA2;           
+using DB4   = mcu::PA3;       
+using DB5   = mcu::PA4;
+using DB6   = mcu::PA5;    
+using DB7   = mcu::PA6;
+
+
+constexpr auto day_of_week = std::array {
+    "no", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вск"
+};
+
+constexpr std::string_view day_of_week_to_string(int i) {
+    return day_of_week[i];
+}
 
 int main()
 {
-    
-    volatile decltype(auto) clock = Clock::make(); 
+    struct Date {
+        uint16_t year   = 20;
+        uint16_t month  = 7;
+        uint16_t day    = 5;
+        uint16_t date   = 30;
+        uint16_t hour   = 14;
+        uint16_t minute = 18;
+        uint16_t second = 0;
+    }date;
+
+    volatile decltype(auto) clock = Clock::make(date.year, date.month, date.day, date.date, date.hour, date.minute, date.second); 
     
     decltype(auto) led = Pin::make<mcu::PC8,mcu::PinMode::Output>();
     Timer timer{200};
+    volatile uint16_t hour{0};
 
-   
+    constexpr auto hd44780_pins = HD44780_pins<RS, RW, E, DB4, DB5, DB6, DB7>{};
+    String_buffer lcd {};
+    HD44780 hd44780 { HD44780::make(hd44780_pins, lcd.get_buffer()) };
 
-    
-
-
-    
     while(1){
       
     led ^= timer.event();
+    lcd.line(0).cursor(4)._10(clock.hour());
+    lcd.line(0).cursor(6) << ":";
+    lcd.line(0).cursor(7)._10(clock.minute());
+    lcd.line(0).cursor(9) << ":";
+    lcd.line(0).cursor(10)._10(clock.second());
+    lcd.line(1).cursor(0) << day_of_week_to_string(clock.day());
+    lcd.line(1).cursor(8)._10(clock.date());
+    lcd.line(1).cursor(10) << ".";
+    lcd.line(1).cursor(11)._10(clock.month());
+    lcd.line(1).cursor(13) << ".";
+    lcd.line(1).cursor(14)._10(clock.year());
+
     //   __WFI();
     }
 
 }
+
+
